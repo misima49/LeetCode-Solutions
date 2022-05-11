de2f5d218890e2599b72795507c54708724d9bfd
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-11 20:31:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.70%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> reqTri;
-        vector<int> curRow;
-        reqTri.push_back({1});
+        vector<vector<int>> reqTri(numRows);
+        reqTri[0] = {1};
         for(int i = 1; i < numRows; i++) {
-            curRow.clear();
             for(int j = 0; j <= i; j++) {
                 if(j == 0 || j == i) {
-                    curRow.push_back(1);
+                    reqTri[i].push_back(1);
                 } else {
-                    curRow.push_back(reqTri[i-1][j-1] + reqTri[i-1][j]);
+                    reqTri[i].push_back(reqTri[i-1][j-1] + reqTri[i-1][j]);
                 }
             }
-            
-            reqTri.push_back(curRow);
         }
         
         return reqTri;
