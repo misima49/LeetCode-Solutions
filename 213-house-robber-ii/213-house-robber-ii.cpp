6dc608fe5b64183ab47691326fac07c651754774
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-12 12:59:33 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (84.48%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    int maxProfit(vector<int>& nums, int ind, int end, vector<int>& dp) {
-        if(ind < end) {
-            return 0;
+    int maxProfit(vector<int>& nums, int start, int end) {
+        int pick, nonPick;
+        int prev = 0, prev2 = 0;
+        int curr;
+        for(int i = start; i < end; i++) {
+            pick = nums[i] + prev2;
+            nonPick = prev;
+            
+            curr = max(pick, nonPick);
+            
+            prev2 = prev;
+            prev = curr;
         }
-        if(ind == end) 
-            return nums[ind];
         
-        if(dp[ind] != -1) return dp[ind];
-        
-        int pick = nums[ind] + maxProfit(nums, ind-2, end, dp);
-        int nonPick = maxProfit(nums, ind-1, end, dp);
-        // cout << ind << " " << pick << " " << nonPick << "\n";
-        return dp[ind] = max(pick, nonPick);
+        return prev;
     }
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         
-        vector<int> dp(nums.size(), -1);
-        int profitWithFirst = maxProfit(nums, nums.size()-1, 1, dp);
-        dp.assign(nums.size(), -1);
-        int profitWithLast = maxProfit(nums, nums.size()-2, 0, dp);
-        // cout << profitWithFirst << " " << profitWithLast;
+        
+        int profitWithFirst = maxProfit(nums, 0, nums.size()-1);
+        int profitWithLast = maxProfit(nums, 1, nums.size());
         return max(profitWithFirst, profitWithLast);
     }
 };
