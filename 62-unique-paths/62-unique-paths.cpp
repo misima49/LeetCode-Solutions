42d3754b26c29c3075a6c6da32a9353f02c45077
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-14 11:36:46 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (57.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int findPath(int x, int y, vector<vector<int>>& dp) {
-        if(x == 0 && y == 0) return 1;
-        
-        if(dp[x][y] != -1) return dp[x][y];
-        
-        int left = 0, up = 0;
-        if(x > 0) {
-            left = findPath(x-1, y, dp);
-        }
-        
-        if(y > 0) {
-            up = findPath(x, y-1, dp);
-        }
-        
-        return dp[x][y] = left+up;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return findPath(m-1, n-1, dp);
+        vector<int> prevRow(n, 0);
+        
+        for(int i = 0; i < m; i++) {
+            vector<int> cur(n, 0);
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) {
+                    cur[j] = 1;
+                    continue;
+                }
+                
+                if(i > 0) cur[j] += prevRow[j];
+                if(j > 0) cur[j] += cur[j-1];
+                
+            }
+            prevRow = cur;
+        }
+        
+        return prevRow[n-1];
     }
 };
