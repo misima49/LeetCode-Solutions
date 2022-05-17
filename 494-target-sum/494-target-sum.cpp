23da0a42c21bc39499cf6cc75cc340b1dfe236a7
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-17 22:39:12 +0530

    Time: 280 ms (37.29%), Space: 65.7 MB (9.62%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,19 +1,38 @@
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+ 
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;             
+        }
+         
+        // If hash1 == hash2, their XOR is zero.
+          return hash1;
+    }
+};
+
 class Solution {
-    int findSum(int ind, int target, vector<int>& nums) {
+    int findSum(int ind, int target, vector<int>& nums, unordered_map<pair<int, int>, int, hash_pair>& dp) {
         if(ind < 0) {
             if(target == 0) return 1;
             return 0;
         }
         
-        int posi = findSum(ind-1, target-nums[ind], nums);
-        int neg = findSum(ind-1, target+nums[ind], nums);
+        if(dp.find({ind, target}) != dp.end()) return dp[{ind, target}];
+        
+        int posi = findSum(ind-1, target-nums[ind], nums, dp);
+        int neg = findSum(ind-1, target+nums[ind], nums, dp);
         
         // cout << posi << " " << neg << "\n";
         
-        return posi + neg;
+        return dp[{ind, target}] = posi + neg;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return findSum(nums.size()-1, target, nums);
+        unordered_map<pair<int, int>, int, hash_pair> dp;
+        return findSum(nums.size()-1, target, nums, dp);
     }
 };
