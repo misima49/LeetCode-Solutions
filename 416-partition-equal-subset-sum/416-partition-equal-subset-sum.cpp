746b069168dcd8a1a9ba87709eaa695513831778
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-18 22:28:16 +0530

    Time: 680 ms (7.84%), Space: 12 MB (51.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -8,13 +8,14 @@ public:
         int n = nums.size();
         
         vector<vector<bool>> dp(n, vector<bool>(target+1, false));
-        // cout << "test2";
+        
         for(int i = 0; i < n; i++) {
             dp[i][0] = true;
         }
+        
         if(nums[0] <= target)
             dp[0][nums[0]] = true;
-        // cout << "test1";
+        
         for(int i = 1; i < n; i++) {
             for(int j = 1; j <= target; j++) {
                 bool notPick = dp[i-1][j];
@@ -23,13 +24,7 @@ public:
                 
                 dp[i][j] = pick || notPick;
             }
-            
-            // for(int k = 0; k < target; k++) {
-            //     cout << dp[i][k] << " ";
-            // }
         }
-        // cout << "test3";
         return dp[n-1][target];
-        
     }
 };
