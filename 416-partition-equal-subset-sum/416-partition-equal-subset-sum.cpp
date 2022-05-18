486eab9a29d81a74d9b6acb547301507638efb57
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-18 22:27:26 +0530

    Time: 397 ms (23.54%), Space: 11.8 MB (63.41%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,29 +1,35 @@
 class Solution {
-    bool findSum(int ind, int target, vector<int>& nums, vector<vector<int>>& dp) {
-        if(ind == 0) {
-            return (nums[ind] == target);
-        }
-        
-        if(target == 0) return true;
-        if(dp[ind][target] != -1) return dp[ind][target];
-        
-        bool notPick = findSum(ind-1, target, nums, dp);
-        bool pick = false;
-        if(nums[ind] <= target) {
-            pick = findSum(ind-1, target-nums[ind], nums, dp);
-        }
-        
-        return dp[ind][target] = pick || notPick;
-    }
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for(int itr : nums) totalSum += itr;
-        if(totalSum % 2 != 0) return false;
+        int target = 0;
+        for(int itr : nums) target += itr;
+        if(target % 2 != 0) return false;
+        target /= 2;
         int n = nums.size();
         
-        vector<vector<int>> dp(n, vector<int>(totalSum/2+1, -1));
+        vector<vector<bool>> dp(n, vector<bool>(target+1, false));
+        // cout << "test2";
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if(nums[0] <= target)
+            dp[0][nums[0]] = true;
+        // cout << "test1";
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= target; j++) {
+                bool notPick = dp[i-1][j];
+                bool pick = false;
+                if(nums[i] < j) pick = dp[i-1][j-nums[i]];
+                
+                dp[i][j] = pick || notPick;
+            }
+            
+            // for(int k = 0; k < target; k++) {
+            //     cout << dp[i][k] << " ";
+            // }
+        }
+        // cout << "test3";
+        return dp[n-1][target];
         
-        return findSum(n-1, totalSum/2, nums, dp);
     }
 };
