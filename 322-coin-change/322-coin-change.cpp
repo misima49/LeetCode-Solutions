3bffee478979f43f2250a82dd170657ed2058a70
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-21 11:17:44 +0530

    Time: 244 ms (11.65%), Space: 45.1 MB (21.35%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,26 +1,44 @@
 class Solution {
-    int findChange(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp) {
-        // if(amount == 0) return 1;
-        if(ind == 0) {
-            if(amount%coins[ind] == 0) return amount/coins[ind];
-            else return 1e8;
-        }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
+//     int findChange(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+//         if(ind == 0) {
+//             if(amount%coins[ind] == 0) return amount/coins[ind];
+//             else return 1e8;
+//         }
+//         if(dp[ind][amount] != -1) return dp[ind][amount];
         
-        int notPick = findChange(ind-1, amount, coins, dp);
-        int pick = 1e8;
-        if(coins[ind] <= amount) pick = 1 + findChange(ind, amount-coins[ind], coins, dp);
+//         int notPick = findChange(ind-1, amount, coins, dp);
+//         int pick = 1e8;
+//         if(coins[ind] <= amount) pick = 1 + findChange(ind, amount-coins[ind], coins, dp);
         
-        return dp[ind][amount] = min(pick, notPick);
-    }
+//         return dp[ind][amount] = min(pick, notPick);
+//     }
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int target) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int answer = findChange(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(target+1, 1e8));
         
+        for(int i = 0; i <= target; i++) {
+            if(i%coins[0] == 0) {
+                dp[0][i] = i/coins[0];
+            }
+        }
+        // cout << "here1\n";
+        for(int i = 1; i < n; i++) {
+            // cout << "here4\n";
+            for(int j = 0; j <= target; j++) {
+                // cout << "here3\n";
+                int notPick = dp[i-1][j];
+                int pick = 1e8;
+                if(coins[i] <= j) pick = 1 + dp[i][j-coins[i]];
+                
+                dp[i][j] = min(pick, notPick);
+            }
+        }
+        // cout << "here2\n";
+        // int answer = findChange(n-1, amount, coins, dp);
         
-        if(answer == 1e8) return -1;
-        return answer;
+        if(dp[n-1][target] == 1e8) return -1;
+        return dp[n-1][target];
+        // return answer;
     }
 };
