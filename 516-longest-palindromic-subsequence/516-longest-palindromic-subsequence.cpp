9e58a956d24f7fb891f27591cd0e8808e3a04e67
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-25 15:12:40 +0530

    Time: 186 ms (36.51%), Space: 6.6 MB (96.33%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -5,18 +5,23 @@ public:
         string s2 = s1;
         reverse(s1.begin(), s1.end());
         
-        vector<vector<int>> dp(len+1, vector<int>(len+1, 0));
+        // vector<vector<int>> dp(len+1, vector<int>(len+1, 0));
+        vector<int> prev(len+1, 0), cur(len+1, 0);
         
         for(int i = 1; i <= len; i++) {
             for(int j = 1; j <= len; j++) {
                 if(s1[i-1] == s2[j-1]) {
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    // dp[i][j] = 1+dp[i-1][j-1];
+                    cur[j] = 1+prev[j-1];
                 } else {
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                    // dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                    cur[j] = max(cur[j-1], prev[j]);
                 }
             }
+            prev = cur;
         }
         
-        return dp[len][len];
+        // return dp[len][len];
+        return prev[len];
     }
 };
