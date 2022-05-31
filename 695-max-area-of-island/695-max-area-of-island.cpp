de5a14d1c8e77e175761bc3a147d8a5e448a36a9
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-05-31 21:43:01 +0530

    Time: 24 ms (63.75%), Space: 23.7 MB (39.98%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,56 +1,52 @@
 class Solution {
+    bool isValid(int r, int c, int rlen, int clen) {
+        if(r < rlen && c < clen && r >= 0 && c >= 0) return true;
+        return false;
+    }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int largestAr = 0;
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                if(grid[i][j] == 1) 
-                    largestAr = max(dfs(grid, i, j), largestAr);
+        int rlen = grid.size();
+        int clen = grid[0].size();
+        int cur, largest = 0;
+        
+        stack<pair<int, int>> st;
+        
+        for(int i = 0; i < rlen; i++) {
+            for(int j = 0; j < clen; j++) {
+                if(grid[i][j] == 1) {
+                    st.push({i, j});
+                    grid[i][j] = 0;
+                    cur = 0;
+                    while(!st.empty()) {
+                        int x = st.top().first;
+                        int y = st.top().second;
+                        st.pop();
+                        // cout << x << " " << y << "\n";
+                        cur++;
+                        
+                        if(isValid(x+1, y, rlen, clen) && grid[x+1][y] == 1) {
+                            st.push({x+1, y});
+                            grid[x+1][y] = 0;
+                        }
+                        if(isValid(x-1, y, rlen, clen) && grid[x-1][y] == 1) {
+                            st.push({x-1, y});
+                            grid[x-1][y] = 0;
+                        }
+                        if(isValid(x, y+1, rlen, clen) && grid[x][y+1] == 1) {
+                            st.push({x, y+1});
+                            grid[x][y+1] = 0;
+                        }
+                        if(isValid(x, y-1, rlen, clen) && grid[x][y-1] == 1) {
+                            st.push({x, y-1});
+                            grid[x][y-1] = 0;
+                        }
+                    }
+                    // cout << "\n";
+                    largest = max(largest, cur);
+                }
             }
         }
         
-        return largestAr;
-    }
-    
-    int dfs(vector<vector<int>>& grid, int x, int y) {
-        stack<pair<int, int>> st;
-        int islandSize = 1;
-        
-        st.push({x, y});
-        grid[x][y] = 0;
-        
-        while(!st.empty()) {
-            x = st.top().first;
-            y = st.top().second;
-            
-            st.pop();
-            
-            if(x < grid.size() - 1 && grid[x+1][y] == 1) {
-                st.push({x+1, y});
-                islandSize++;
-                grid[x+1][y] = 0;
-            } 
-            
-            if(x > 0 && grid[x-1][y] == 1) {
-                st.push({x-1, y});
-                islandSize++;
-                grid[x-1][y] = 0;
-            } 
-            
-            if(y < grid[0].size() - 1 && grid[x][y+1] == 1) {
-                st.push({x, y+1});
-                islandSize++;
-                grid[x][y+1] = 0;
-            } 
-            
-            if(y > 0 && grid[x][y-1] == 1) {
-                st.push({x, y-1});
-                islandSize++;
-                grid[x][y-1] = 0;
-            } 
-        }
-        
-        cout << islandSize << "\n";
-        return islandSize;
+        return largest;
     }
 };
