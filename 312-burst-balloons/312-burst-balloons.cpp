c9b923a53bfb0fd8cf0c6f14122b65a88eb88130
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-04 17:32:51 +0530

    Time: 567 ms (52.75%), Space: 10.3 MB (69.29%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -17,11 +17,28 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();    
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
         
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         
-        return findCoins(1, n, nums, dp);
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= n; j++) {
+                if(i > j) continue;
+                
+                int maxi = -1e8;
+                for(int itr = i; itr <= j; itr++) {
+                    int cost = nums[i-1]*nums[itr]*nums[j+1] +
+                        dp[i][itr-1] + dp[itr+1][j];
+                    
+                    maxi = max(cost, maxi);
+                }
+                
+                dp[i][j] = maxi;
+            }
+        }
+        
+        // return findCoins(1, n, nums, dp);
+        return dp[1][n];
     }
 };
