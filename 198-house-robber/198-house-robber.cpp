76d1acabe1b24cbf30b54d0ccf578cf4188b4416
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-05 11:06:47 +0530

    Time: 10 ms (5.27%), Space: 7.6 MB (94.90%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,39 +1,24 @@
 class Solution {
-    int findMoney(int ind, bool canRob, vector<int>& nums, vector<vector<int>>& dp) {
-        int n = nums.size();
-        if(ind == n) return 0;
-        
-        if(dp[ind][canRob] != -1) return dp[ind][canRob];
-        
-        int pick = 0;
-        int notPick = 0;
-        if(canRob) {
-            pick = nums[ind] + findMoney(ind+1, !canRob, nums, dp);
-            notPick = findMoney(ind+1, canRob, nums, dp);
-        } else {
-            notPick = findMoney(ind+1, !canRob, nums, dp);
-        }
-        // cout << pick << "\n";
-        return dp[ind][canRob] = max(pick, notPick);
-    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        vector<int> next(2, 0), cur(2, 0);
+        
         
         
         for(int i = n-1; i >= 0; i--) {
             int pick = 0;
             int notPick = 0;
-            pick = nums[i] + dp[i+1][false];
-            notPick = dp[i+1][true];
+            pick = nums[i] + next[false];
+            notPick = next[true];
             
-            dp[i][true] = max(pick, notPick);
-            dp[i][false] = notPick;
+            cur[true] = max(pick, notPick);
+            cur[false] = notPick;
+            
+            next = cur;
             
         }
         
-        return dp[0][true];
-        // return findMoney(0, true, nums, dp);
+        return next[true];
     }
 };
