8a3f9243ae1691c887331f154a929ad85b559cdd
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-05 11:48:51 +0530

    Time: 8 ms (60.21%), Space: 9 MB (26.36%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,38 +1,19 @@
 class Solution {
-    int findPath(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        if(i >= triangle.size()) return 0;
+    int findSum(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+        int n = triangle.size();
+        if(i == n) return 0;;
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        int down = triangle[i][j] + findPath(i+1, j, triangle, dp);
-        int diag = triangle[i][j] + findPath(i+1, j+1, triangle, dp);
+        int down = triangle[i][j] + findSum(i+1, j, triangle, dp);
+        int diag = triangle[i][j] + findSum(i+1, j+1, triangle, dp);
         
         return dp[i][j] = min(down, diag);
     }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<int> prev(triangle.size());
-        vector<int> cur(triangle.size());
-        
-        int i = triangle.size() - 1;
-        int j;
-        
-        for( ; i >= 0; i--) {
-            for(j = i ; j >= 0; j--) {
-                if(i == triangle.size()-1) {
-                    cur[j] = triangle[i][j];
-                    continue;
-                }
-                
-                int down = triangle[i][j] + prev[j];
-                int diag = triangle[i][j] + prev[j+1];
-                
-                cur[j] = min(down, diag);
-            }
-            
-            prev = cur;
-        }
-        
-        return prev[0];
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return findSum(0, 0, triangle, dp);
     }
 };
