fd87c1f13941b8fd0804f91b7f887734ec887053
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-05 11:02:31 +0530

    Time: 6 ms (13.94%), Space: 8.1 MB (6.43%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -19,8 +19,21 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        return findMoney(0, true, nums, dp);
+        
+        for(int i = n-1; i >= 0; i--) {
+            int pick = 0;
+            int notPick = 0;
+            pick = nums[i] + dp[i+1][false];
+            notPick = dp[i+1][true];
+            
+            dp[i][true] = max(pick, notPick);
+            dp[i][false] = notPick;
+            
+        }
+        
+        return dp[0][true];
+        // return findMoney(0, true, nums, dp);
     }
 };
