bcd2c2a6c0146f68675b2095d3f9aed38017c7ea
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-07 11:40:18 +0530

    Time: 60 ms (32.61%), Space: 36.2 MB (21.40%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         int count = 0;
         int maxCount = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == 1) count++;
+        for(int i:nums) {
+            if(i == 1) count++;
             else count = 0;
             
             if(count > maxCount) maxCount = count;
