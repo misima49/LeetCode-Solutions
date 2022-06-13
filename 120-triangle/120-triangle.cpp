2f756ae184bfabb4623876526bacd27b035e9178
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-13 08:49:11 +0530

    Time: 11 ms (37.61%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,15 +1,4 @@
 class Solution {
-    int findSum(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        int n = triangle.size();
-        if(i == n) return 0;;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = triangle[i][j] + findSum(i+1, j, triangle, dp);
-        int diag = triangle[i][j] + findSum(i+1, j+1, triangle, dp);
-        
-        return dp[i][j] = min(down, diag);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
