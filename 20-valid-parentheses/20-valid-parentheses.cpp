74ec7e5ad0a44f1fbee4160fba09ae9df24740e4
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-13 10:50:45 +0530

    Time: 8 ms (6.10%), Space: 6.4 MB (10.20%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s) {
+            cout << c;
+            switch(c) {
+                case ')' : 
+                    if(!st.empty() && st.top() == '(')
+                        st.pop();
+                    else 
+                        return false;
+                    break;
+                    
+                case '}' :
+                    if(!st.empty() && st.top() == '{')
+                        st.pop();
+                    else
+                        return false;
+                    break;
+                    
+                case ']' : 
+                    if(!st.empty() && st.top() == '[')
+                        st.pop();
+                    else 
+                        return false;
+                    break;
+                    
+                default :
+                    st.push(c);
+            }
+        }
+        
+        if(st.empty()) return true;
+        return false;
+    }
+};
