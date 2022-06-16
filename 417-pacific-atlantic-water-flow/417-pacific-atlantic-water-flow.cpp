9a98cf74e2fe05b23cb8f3631077b17900ec86c3
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-16 17:46:16 +0530

    Time: 212 ms (10.45%), Space: 36.8 MB (7.59%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,38 +1,40 @@
 #define dir vector<int>({0, 1, 0, -1, 0})
-struct hashFunction
-{
-    size_t operator()(const pair<int, int> &x) const
-    {
-        return x.first ^ x.second;
-    }
-};
-
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        unordered_set<pair<int, int>, hashFunction> atlan;
-        unordered_set<pair<int, int>, hashFunction> pacif;
+        vector<vector<bool>> atlan_visit(m, vector<bool>(n, false)), 
+            pacif_visit(m, vector<bool>(n, false));
+        queue<pair<int, int>> atlan_q, pacif_q;
         
         for(int i = 0; i < n; i++) {
-            dfs(0, i, pacif, heights);
-            dfs(m-1, i, atlan, heights);
+            pacif_visit[0][i] = true;
+            pacif_q.push({0, i});
+            
+            atlan_visit[m-1][i] = true;
+            atlan_q.push({m-1, i});
         }
         
-        for(int i = 0; i < m; i++) {
-            dfs(i, 0, pacif, heights);
-            dfs(i, n-1, atlan, heights);
+        for(int j = 0; j < m; j++) {
+            pacif_visit[j][0] = true;
+            pacif_q.push({j, 0});
+            
+            atlan_visit[j][n-1] = true;
+            atlan_q.push({j, n-1});
         }
+        
+        bfs(pacif_q, pacif_visit, heights);
+        bfs(atlan_q, atlan_visit, heights);
         
         vector<vector<int>> ans;
         
-        auto itr = atlan.begin();
-        while(itr != atlan.end()) {
-            if(pacif.find(*itr) != pacif.end()) {
-                ans.push_back({itr->first, itr->second});
+        for(int i = 0;  i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(atlan_visit[i][j] && pacif_visit[i][j]) {
+                    ans.push_back({i, j});
+                }
             }
-            itr++;
         }
         
         return ans;
@@ -44,19 +46,23 @@ private:
         return true;
     }
     
-    void dfs(int R, int C, unordered_set<pair<int, int>, hashFunction>& ocean,
-             vector<vector<int>>& heights) {
-        // cout << R << " " << C << " " << heights[R][C] << "\n";
+    void bfs(queue<pair<int, int>>& q, vector<vector<bool>>& visited,
+                    vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        ocean.insert({R, C});
-        
-        for(int i = 0; i < 4; i++) {
-            int x = R+dir[i];
-            int y = C+dir[i+1];
-            if(isValid(x, y, m, n) && heights[R][C] <= heights[x][y]
-               && ocean.find({x, y}) == ocean.end()) {
-                dfs(x, y, ocean, heights);
+
+        while(!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++) {
+                int x = r+dir[i];
+                int y = c+dir[i+1];
+                if(isValid(x, y, m, n) && heights[r][c] <= heights[x][y]
+                  && !visited[x][y]) {
+                    visited[x][y] = true;
+                    q.push({x, y});
+                }
             }
         }
     }
