c19d2ca7dab89988524ea532ba206d090e23eb40
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-17 22:15:16 +0530

    Time: 401 ms (8.07%), Space: 84.1 MB (5.81%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,42 +1,29 @@
 #define dir vector<int>({0, 1, 0, -1, 0})
 
+
 class Solution {
-    bool isValid(int r, int c, int m, int n) {
-        if(r < 0 || c < 0 || r >= m || c >= n) return false;
-        return true;
-    }
-    int findLIS(int r, int c, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-        // cout << r << " " << c << "\n";
-        int m = matrix.size();
-        int n = matrix[0].size();
-        if(r >= m || c >= n || r < 0 || c < 0) return 0;
+    int dp[200][200]{};
+    int ans, m, n;
+    int findLIS(int r, int c, vector<vector<int>>& matrix, int prev) {
+            if(r >= m || c >= n || r < 0 || c < 0 || matrix[r][c] <= prev) return 0;
         
-        if(dp[r][c] != -1) return dp[r][c];
-        
-        int curPathLen = 1;
+        if(dp[r][c]) return dp[r][c];
         for(int i = 0; i < 4; i++) {
             int x = r+dir[i];
             int y = c+dir[i+1];
-            if(isValid(x, y, m, n) && matrix[x][y] > matrix[r][c]) {
-                curPathLen = max(curPathLen, 1 + findLIS(x, y, matrix, dp));
-            }
+            dp[r][c] = max(dp[r][c], findLIS(x, y, matrix, matrix[r][c]));
         }
         
-        return dp[r][c] = curPathLen;
+        return ++dp[r][c];
     }
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int ans = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(dp[i][j] == -1) 
-                    ans = max(ans, findLIS(i, j, matrix, dp));
-            }
-        }
-        
+        m = matrix.size();
+        n = matrix[0].size();
+        ans = 0;
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++) 
+                ans = max(ans, findLIS(i, j, matrix, -1));
         
         return ans;
     }
