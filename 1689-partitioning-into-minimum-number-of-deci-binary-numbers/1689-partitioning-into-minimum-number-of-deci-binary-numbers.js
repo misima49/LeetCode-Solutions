8b333a0147b152c6ac6b784a33021878f405c916
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-27 12:26:23 +0530

    Time: 113 ms (72.31%), Space: 45.9 MB (83.39%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -6,7 +6,7 @@ var minPartitions = function(n) {
     let largestDig = n[0]
     for(ch of n) {
         if(ch > largestDig) largestDig = ch
+        if(largestDig === '9') return largestDig
     }
-    
     return largestDig
 };
