3dd55f646a3bf217db4f364ee6f367f748e5ed7b
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-29 21:05:55 +0530

    Time: 41 ms (5.66%), Space: 24.5 MB (5.56%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<string> ans;
+    void findCombs(int op, int ed, int n, string curS) {
+        if(op == n && ed == n) {
+            ans.push_back(curS);
+            return;
+        } else if(ed > op) return;
+        
+        if(op <= n) {
+            curS.push_back('(');
+            findCombs(op+1, ed, n, curS);
+            curS.pop_back();
+        }
+        
+        if(ed <= op) {
+            curS.push_back(')');
+            findCombs(op, ed+1, n, curS);
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        findCombs(0, 0, n, "");
+        return ans;
+    }
+};
