8498078cf35731c1e4a75c41064d4e82d83a75b2
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-30 12:27:32 +0530

    Time: 72 ms (88.75%), Space: 43.6 MB (92.50%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
@@ -6,7 +6,7 @@ var minMoves2 = function(nums) {
     let st = 0
     let ed = nums.length-1
     let ans = 0
-    nums.sort((a, b) => {return a-b})
+    nums.sort((a, b) => a-b)
     while(st < ed) {
         ans += nums[ed] - nums[st]
         ed--
