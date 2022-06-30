9a1575c42d306688d50bb1611f8079898c3c69cf
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-06-30 12:25:07 +0530

    Time: 98 ms (41.25%), Space: 44.5 MB (22.50%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function(nums) {
+    let st = 0
+    let ed = nums.length-1
+    let ans = 0
+    nums.sort((a, b) => {return a-b})
+    while(st < ed) {
+        ans += nums[ed] - nums[st]
+        ed--
+        st++
+    }
+    
+    return ans
+};
