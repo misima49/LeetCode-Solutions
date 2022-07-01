8685b9bb19d903ba550343f0e975b9e58e7f2376
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-01 11:49:35 +0530

    Time: 10 ms (93.40%), Space: 11.7 MB (90.15%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,33 +1,23 @@
 class Solution {
-    int findPoints(int ind, vector<int>& nums, vector<int>& dp) {
-        int n = nums.size();
-        if(ind == n) return 0;
-        
-        if(dp[ind] != -1) return dp[ind];
-        
-        int notPick = 0, pick = 0;
-        int nextPoint;
-        for(nextPoint = ind; nextPoint < n && nums[nextPoint] == nums[ind]; nextPoint++);
-        
-        int curPoints = (nextPoint-ind)*nums[ind];
-        notPick = findPoints(nextPoint, nums, dp);
-        for(; nextPoint < n && nums[nextPoint] == nums[ind]+1; nextPoint++);
-        pick = curPoints + findPoints(nextPoint, nums, dp);
-        
-        return dp[ind] = max(pick, notPick);
-    }
 public:
     int deleteAndEarn(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-//         int nex = 0, nex2 = 0;
+        vector<int> dp(n+1, 0);
         
-//         for(int i = n-1; i >= 0; i--) {
+        for(int i = n-1; i >= 0; i--) {
+            int nexPoint = i;
             
-//         }
+            while(nexPoint < n && nums[nexPoint] == nums[i]) nexPoint++;
+            int points = (nexPoint-i)*nums[i];
+            int notPick = dp[nexPoint];
+            
+            while(nexPoint < n && nums[nexPoint] == nums[i]+1) nexPoint++;
+            int pick = points + dp[nexPoint];
+            
+            dp[i] = max(pick, notPick);
+        }
         
-        vector<int> dp(n, -1);
-        
-        return findPoints(0, nums, dp);
+        return dp[0];
     }
 };
