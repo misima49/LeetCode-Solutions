fde766ea8f0195f1cc4c4395afd656e6e5202e34
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-09 13:12:22 +0530

    Time: 1065 ms (23.75%), Space: 98.3 MB (96.79%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -15,26 +15,23 @@ class Solution {
         
         return lastFound;
     }
-    
-    long long findEarnings(int ind, vector<vector<int>>& rides, vector<long long>& dp) {
-        if(ind >= rides.size()) return 0;
-        
-        if(dp[ind] != -1) return dp[ind];
-        
-        long long notPick = findEarnings(ind+1, rides, dp);
-        
-        int nexPossible = searchRide(rides, rides[ind][1]);
-        long long pick = rides[ind][1] - rides[ind][0] + rides[ind][2] + findEarnings(nexPossible, rides, dp);
-        
-        return dp[ind] = max(pick , notPick);
-    }
 public:
     long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
         sort(rides.begin(), rides.end());
   
         int sz = rides.size();
-        vector<long long> dp(sz, -1);
+        vector<long long> dp(sz+1, 0);
         
-        return findEarnings(0, rides, dp);
+        for(int ind = sz-1; ind >= 0; ind--) {
+            long long notPick = dp[ind+1];
+        
+            int nexPossible = searchRide(rides, rides[ind][1]);
+            long long pick = rides[ind][1] - rides[ind][0] + rides[ind][2] + dp[nexPossible];
+
+            dp[ind] = pick > notPick ? pick : notPick;
+        }
+        
+        
+        return dp[0];
     }
 };
