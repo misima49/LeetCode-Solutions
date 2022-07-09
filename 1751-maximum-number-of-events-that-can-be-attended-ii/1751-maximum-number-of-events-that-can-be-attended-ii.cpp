8a3b708cce59e8d8149f3ad86f4f174990bad236
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-09 15:04:55 +0530

    Time: 392 ms (72.46%), Space: 63.4 MB (85.53%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -14,21 +14,7 @@ class Solution {
             }
         }
         
-        // cout << prevEd << " " << lastFound << "\n";
         return lastFound;
-    }
-    
-    int findAttendable(int ind, vector<vector<int>>& events, int k, vector<vector<int>>& dp) {
-        int n = events.size();
-        if(ind >= n || k == 0) return 0;
-        if(dp[ind][k] != -1) return dp[ind][k];
-        
-        int notAttend = findAttendable(ind+1, events, k, dp);
-
-        int nexEvent = findNext(events, events[ind][1]+1);
-        int attend = findAttendable(nexEvent, events, k-1, dp) + events[ind][2];
-        
-        return dp[ind][k] = max(attend, notAttend);
     }
 public:
     int maxValue(vector<vector<int>>& events, int k) {
@@ -48,7 +34,5 @@ public:
         }
         
         return dp[0][k]+1;
-        
-        // return findAttendable(0, events, k, dp);
     }
 };
