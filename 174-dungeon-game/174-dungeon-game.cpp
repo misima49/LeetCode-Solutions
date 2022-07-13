ce549258c86ea4b27c8c864e21db0c5885474860
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-13 14:03:43 +0530

    Time: 6 ms (87.87%), Space: 8.8 MB (73.98%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -21,35 +21,21 @@ public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m = dungeon.size();
         int n = dungeon[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
-//         for(int i = 0; i <= n; i++) {
-//             dp[m-1][i] = -1e8;
-//         }
-        
-//         for(int i = 0; i <= m; i++) {
-//             dp[i][n-1] = -1e8;
-//         }
-        
-//         dp[m-1][n-1] = dungeon[m-1][n-1];
-        
-//         for(int r = m-1; r >= 0; r--) {
-//             for(int c = n-1; c >= 0; c--) {
-//                 int right = dp[r][c+1];
-//                 int down = dp[r+1][c];
+        vector<int> prevRow(n+1, -1e8);
 
-//                 int maxHealth = max(right, down);
+        prevRow[n-1] = dungeon[m-1][n-1];
 
-//                 dp[r][c] = dungeon[r][c];
-//                 if(dungeon[r][c] >= 0 || maxHealth < 0) dp[r][c] += maxHealth;
-//             }
-//         }
-        
-//         cout << dp[0][0];
-//         return max(-dp[0][0], 0)+1;
-        int ans = findPath(0, 0, dungeon, dp);
-        if(ans <= 0) return -ans + 1;
-        
-        return 1;
+        for(int r = m-1; r >= 0; r--) {
+            for(int c = n-1; c >= 0; c--) {
+                if(r == m-1 && c == n-1) continue;
+
+                int maxHealth = max(prevRow[c], prevRow[c+1]);
+                prevRow[c] = dungeon[r][c];
+                if(dungeon[r][c] >= 0 || maxHealth < 0) prevRow[c] += maxHealth;
+            }
+        }
+
+        return max(-prevRow[0], 0)+1;
     }
 };
