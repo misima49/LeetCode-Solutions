3e15cc3c6b9f248f9811f07609e599e18dfb058d
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-14 17:40:02 +0530

    Time: 149 ms (23.78%), Space: 41.9 MB (89.55%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -5,16 +5,13 @@ public:
 
         int boatCount = 0;
         int st = 0, ed = people.size()-1;
-        while(st < ed) {
+        while(st <= ed) {
             if(people[st]+people[ed] <= limit) {
                 st++;
-                ed--;
-            } else {
-                ed--;
-            }
+            } 
+            ed--;
             boatCount++;
-        }
-        if(st == ed) boatCount++;
+        }        
         
         return boatCount;
     }
