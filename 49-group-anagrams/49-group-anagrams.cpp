1bbabce8dca4a951add5c802e9e3efb1f37be2ee
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-19 12:04:44 +0530

    Time: 35 ms (92.92%), Space: 18.8 MB (90.68%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,23 +1,10 @@
 class Solution {
-    void strSort(string& s) {
-        vector<int> alpha(26, 0);
-        for(char& ch : s) {
-            alpha[ch - 'a']++;
-        }
-        
-        s.clear();
-        for(int i = 0; i < 26; i++) {
-            while(alpha[i]--) {
-                s.push_back('a'+i);
-            }
-        }
-    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> hash;
         for(string& s : strs) {
             string t = s;
-            strSort(t);
+            sort(t.begin(), t.end());
             hash[t].push_back(s);
         }
         
