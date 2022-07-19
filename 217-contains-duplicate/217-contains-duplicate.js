1683eab6dd9325cbdd74317927100008117f7c56
Mrinal Singh Malav <56494010+misima49@users.noreply.github.com>
2022-07-19 11:31:29 +0530

    Time: 94 ms (89.95%), Space: 50.7 MB (63.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -4,11 +4,10 @@
  */
 var containsDuplicate = function(nums) {
     let exist = new Set();
-    let ans = 0;
     for(let itr of nums) {
-        if(exist.has(itr)) ans++;
-        else exist.add(itr);
+        if(exist.has(itr)) return true;
+        exist.add(itr);
     }
     
-    return ans;
+    return false;
 };
